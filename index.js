66369e5e9a1aa5ecee3642ad2240d6d9738564dd
Evandro <juniok590@gmail.com>
2022-10-04 18:42:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const categorias = require("./categorias.json");
+const resumo = require("./resumo.json");
+
+app.get("/categorias", (req, res) => {
+  return res.json(categorias);
+});
+app.get("/resumo", (req, res) => {
+  return res.json(resumo);
+});
+
+app.listen(port, () => {
+  console.log("listeners listening on port " + port);
+});
